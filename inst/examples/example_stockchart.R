315fe82ace735b8384d9484267bfb3da48498767
timelyportfolio <kent.russell@timelyportfolio.com>
2015-09-10 16:23:45 -0500

    add chartSeries plot in example for reference and testing

--- inst/examples/example_stockchart.R
+++ inst/examples/example_stockchart.R
@@ -6,4 +6,7 @@ spy <- getSymbols("SPY", auto.assign = F)
 stockchart(tail(spy,200))
 
 aapl <- getSymbols("AAPL", auto.assign = FALSE)
+# look at the static plot for reference
+chartSeries(adjustOHLC(aapl),theme="white")
+# now make an interactive plot
 stockchart(adjustOHLC(aapl))
